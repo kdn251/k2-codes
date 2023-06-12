6094e695d2ee5fdc077713cc8e4d06b76e8c82ac
Kevin Naughton Jr <kdn251@nyu.edu>
2023-06-11 20:58:26 -0400

    Add links and homepage

---
+++ src/pages/onlyfans.tsx
@@ -0,0 +1,19 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { api } from "~/utils/api";
+
+const OnlyFansPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>lul</title>
+      </Head>
+      <main>
+        <h1 className="text-4xl text-center pb-2">fr???</h1>
+      </main>
+    </>
+  );
+};
+
+export default OnlyFansPage;
