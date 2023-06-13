bb648b4e1b0ca6cdf339e85de0855b3884fdb1e1
Kevin Naughton Jr <kdn251@nyu.edu>
2023-06-13 00:09:34 -0400

    fix errors

--- src/pages/onlyfans.tsx
+++ src/pages/onlyfans.tsx
@@ -5,23 +5,12 @@ import { api } from "~/utils/api";
 const OnlyFansPage: NextPage = () => {
   const PageLoadCount = () => {
     const count = api.count.incrementCount.useMutation();
-    console.log('count: ' + count);
     const { data } = api.count.getCount.useQuery();
     if (!data) {
       return <div className="text-center">Loading...</div>
     }
     return <h1 className="text-3xl text-center pb-2">{data?.count}</h1>
   }
-  // let newCount = data?.count + 1;
-  // const { mutate } = api.count.incrementCount.useMutation({
-  //   onSuccess: () => {
-  //     console.log('yay');
-  //   },
-  //   onError: (e) => {
-  //     console.log('oof');
-  //   },
-  // });
-  // mutate({ newCount: initialCount + 1});
 
   return (
     <>
--- src/server/api/routers/count.ts
+++ src/server/api/routers/count.ts
@@ -5,16 +5,4 @@ export const countRouter = createTRPCRouter({
   getCount: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.uniqueVisitor.findFirst();
   }),
-  // incrementUniqueVisitors: publicProcedure.mutation((opts) => {
-  //     return opts.ctx.prisma.uniqueVisitor.create;
-  // }),
-  incrementCount: publicProcedure.input(z.object({ newCount: z.number()})).mutation(({ctx, input}) => {
-      console.log('new count: ' + input.newCount);
-      const currentCount = ctx.prisma.uniqueVisitor.findFirst();
-      ctx.prisma.uniqueVisitor.update({
-        where: { id: 1 },
-        data: { count: input?.newCount + 1 },
-      })
-      return currentCount;
-  }),
 });
