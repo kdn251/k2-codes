d14dd4f0fd21066cb1cccbaed6353b9d8dbdad9e
Kevin Naughton Jr. <kdn251@nyu.edu>
2025-01-23 22:11:01 -0500

    manual revert with commit

--- src/pages/onlyfans.tsx
+++ src/pages/onlyfans.tsx
@@ -1,29 +1,24 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { useEffect } from "react";
 
 const OnlyFansPage: NextPage = () => {
-  const incrementedCount = api.count.incrementCount.useMutation();
-
-  useEffect(() => {
-    incrementedCount.mutate(); 
-  }, []);
-
-  const { data } = api.count.getCount.useQuery();
-  if (!data) {
-    return <div className="text-center">Loading...</div>;
+  const PageLoadCount = () => {
+    const { data } = api.count.getCount.useQuery();
+    if (!data) {
+      return <div className="text-center">Loading...</div>
+    }
+    return <h1 className="text-3xl text-center pb-2">{data?.count}</h1>
   }
+
   return (
     <>
       <Head>
         <title>lul</title>
       </Head>
       <main>
-        <h1 className="text-3xl text-center pb-2">{data?.count}</h1>
-        <p className="text-center">
-          people have searched for kevin&apos;s OnlyFans page 🥵
-        </p>
+        <PageLoadCount />
+        <p className="text-center">people have searched for kevin&apos;s OnlyFans page 🥵</p>
       </main>
     </>
   );
